761006b2528ecb1579fba2b23ad96d4313e98d88
hainguyen0910 <nguyencn17a@gmail.com>
2020-12-27 23:24:32 +0700

    config project

---
+++ alo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+import './style.css';
+
+function alo() {
+  return (
+    <div className="test">
+      This is alo 123
+    </div>
+  )
+}
+
+export default alo
