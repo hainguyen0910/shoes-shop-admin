b9a542c849e59680e402787a9e057d33c41789ed
hainguyen0910 <nguyencn17a@gmail.com>
2020-12-27 23:36:24 +0700

    config project

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.scss';
+
+function App() {
+  return (
+    <div className="test">
+      This is App 123
+      <div className="btn btn-primary">
+        alo
+      </div>
+    </div>
+  )
+}
+
+export default App
